2f619513d6796434486d9daf00be16261501d73c
Mahesh Dadwal <maheshdadwal07@gmail.com>
2026-02-10 11:22:33 +0530

    Add health check API

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    is_success: true,
+    official_email: "mahesh0562.be23@chitkara.edu.in"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
